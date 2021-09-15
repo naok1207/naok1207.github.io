e691680cb75eb68f11432db918c444a2c3968794
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-09-15 06:13:49 +0000

    add home and default component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-// import 文を使って sub.js ファイルを読み込む。
-import { hello } from "./sub";
-
-// sub.jsに定義されたJavaScriptを実行する。
-hello();
-
-console.log("aaa");
-console.log("bbb");
-// console.log("ccc");
+import "tailwindcss/tailwind.css"
+import "./style.css"
