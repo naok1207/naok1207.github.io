2507f3f5db105d1985016990cec0f2c46c630637
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-09-15 08:57:15 +0000

    save commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import "tailwindcss/tailwind.css"
 import "./style.css"
+
+var style = document.createElement("style");
+document.head.appendChild( style );
+var sheet = style.sheet;
+
+const sections = document.getElementsByTagName('section');
+const sectionsLength = sections.length
+
+for(var i = 0; i < sectionsLength; i++) {
+  sheet.insertRule(sectionBeforeCss(sections.item(i).id), i);
+}
+
+function sectionBeforeCss(id) {
+  return `#${id}::before {
+    content: ${id};
+    position: absolute;
+    left: 5.9rem;
+    top: 50%;
+    transform: rotate(-90deg) translateY(-50%);
+    color: white;
+    z-index: 50;
+  }`
+}
