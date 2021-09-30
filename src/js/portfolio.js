d6f9c2349447d0b9a7a1626dc7ecd157cc06d7c4
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-09-30 02:35:00 +0000

    add portfolio sample page

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -5,17 +5,16 @@ portfolioCards.forEach(function(card, index){
   card.addEventListener('click', function() {
     var activePortfolio = document.querySelector('.portfolio.show');
     portfolioHidden(activePortfolio);
-    portfolioShow(portfolios[index]);
+    setTimeout(portfolioShow, 500, portfolios[index]);
   })
 })
 
-function portfolioShow(portfolio) {
-  console.log(portfolio);
+var portfolioShow = (portfolio) => {
   portfolio.classList.remove('hidden');
   portfolio.classList.add('show');
 }
 
-function portfolioHidden(portfolio) {
+var portfolioHidden = (portfolio) => {
   portfolio.classList.remove('show');
   portfolio.classList.add('hidden');
 }
